2b40842ad779aacd00f2e5da07635ec892e7fc9f
Brett Feltmate <brettfeltmate@TB757.WPA.Dal.Ca>
2021-08-31 10:55:43 -0300

    Fixed typos in schema.sql

--- ExpAssets/Config/CompensatoryTrackingTask_schema.sql
+++ ExpAssets/Config/CompensatoryTrackingTask_schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE trials (
     id integer primary key autoincrement not null,
     participant_id integer not null references participants(id),
     block_num integer not null,
-    trial_num integer not null
+    trial_num integer not null,
     timestamp text not null,
     buffeting_force text not null,
     additional_force text not null,
@@ -31,12 +31,11 @@ CREATE TABLE assessments (
 	timestamp text not null,
 	buffeting_force integer not null,
 	additional_force integer not null,
-	net_force integer not null
+	net_force integer not null,
 	user_input integer not null,
 	displacement integer not null,
 	rt integer not null
-
-)
+);
 
 CREATE TABLE frames (
 	id integer primary key autoincrement not null,
@@ -47,6 +46,4 @@ CREATE TABLE frames (
 	mean_rt integer not null,
 	lapses integer not null,
 	samples integer not null
-)
-
-		return [self.timestamp, self.forces['buffeting'], self.forces['additional'], self.forces['net'], self.user_input, self.displacement, self.rt]
+);
